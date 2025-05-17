b9c145ef62d1f12aaeaf79deaaee39720750e895
vinchestro <zabrod478@gmail.com>
2025-05-17 15:54:43 +0300

    fixed up migration

--- internal/ratelimit/migrations/000001_create_user_config_table.up.sql
+++ internal/ratelimit/migrations/000001_create_user_config_table.up.sql
@@ -1,1 +1,7 @@
-DROP TABLE IF EXISTS user_configs;
+CREATE TABLE IF NOT EXISTS user_configs (
+ip varchar() PRIMARY KEY,
+capacity int NOT NULL,
+rate_per_sec float64 NOT NULL,
+updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
